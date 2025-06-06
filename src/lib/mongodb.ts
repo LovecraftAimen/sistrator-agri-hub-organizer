91fb79e6201e1939add976cb28acee4ac5410fdb
LovecraftAimen <yan.hellen.2024@gmail.com>
2025-06-05 23:53:31 -0300

    Complete MongoDB Migration

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -30,3 +30,66 @@ export async function getCollection(collectionName: string) {
   const client = await connectToDatabase();
   return client.db("sistrator").collection(collectionName);
 }
+
+// Database operations for beneficiaries
+export async function getBeneficiarios() {
+  const collection = await getCollection('beneficiarios');
+  return collection.find({}).toArray();
+}
+
+export async function addBeneficiario(beneficiario: any) {
+  const collection = await getCollection('beneficiarios');
+  return collection.insertOne(beneficiario);
+}
+
+export async function updateBeneficiario(id: string, beneficiario: any) {
+  const collection = await getCollection('beneficiarios');
+  return collection.updateOne({ _id: id }, { $set: beneficiario });
+}
+
+export async function deleteBeneficiario(id: string) {
+  const collection = await getCollection('beneficiarios');
+  return collection.deleteOne({ _id: id });
+}
+
+// Database operations for services
+export async function getServicos() {
+  const collection = await getCollection('servicos');
+  return collection.find({}).toArray();
+}
+
+export async function addServico(servico: any) {
+  const collection = await getCollection('servicos');
+  return collection.insertOne(servico);
+}
+
+export async function updateServico(id: string, servico: any) {
+  const collection = await getCollection('servicos');
+  return collection.updateOne({ _id: id }, { $set: servico });
+}
+
+export async function deleteServico(id: string) {
+  const collection = await getCollection('servicos');
+  return collection.deleteOne({ _id: id });
+}
+
+// Database operations for tratoristas
+export async function getTratoristas() {
+  const collection = await getCollection('tratoristas');
+  return collection.find({}).toArray();
+}
+
+export async function addTratorista(tratorista: any) {
+  const collection = await getCollection('tratoristas');
+  return collection.insertOne(tratorista);
+}
+
+export async function updateTratorista(id: string, tratorista: any) {
+  const collection = await getCollection('tratoristas');
+  return collection.updateOne({ _id: id }, { $set: tratorista });
+}
+
+export async function deleteTratorista(id: string) {
+  const collection = await getCollection('tratoristas');
+  return collection.deleteOne({ _id: id });
+}
